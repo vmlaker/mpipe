17107283205f6e10db382568e27f143ecdb16dcd
vmlaker <velimir.mlaker@gmail.com>
2013-06-10 12:33:11 -0700

    Specify title.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -113,8 +113,7 @@ html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = '%s documentation'%project
-html_title = 'Documentation'
+html_title = '{0} Documentation'.format(project)
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
