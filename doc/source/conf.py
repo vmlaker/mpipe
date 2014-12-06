ff5e13a6212eacd5416503b47036f26a237a4b07
vmlaker <velimir.mlaker@gmail.com>
2014-12-06 14:01:42 -0800

    Increment copyright year.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MPipe'
-copyright = u'2013, Velimir Mlaker'
+copyright = u'2014, Velimir Mlaker'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
