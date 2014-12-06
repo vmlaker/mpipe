5a1804cf64271931f0cd3e4fff3e2b38291212dd
vmlaker <velimir.mlaker@gmail.com>
2014-12-06 15:04:10 -0800

    Update gh-pages build script.

--- doc/create-gh-pages.sh
+++ doc/create-gh-pages.sh
@@ -3,30 +3,31 @@
 # Start at the root directory.
 cd ..
 
-# Create a Python Virtualenv in doc/ directory.
-virtualenv doc/venv
-doc/venv/bin/python setup.py install
-doc/venv/bin/pip install -r doc/requirements.txt
-
 # Switch to gh-pages branch and start afresh.
 git checkout gh-pages
 rm -rf *
 
-# Switch to master branch (Sphinx build needs doc/ test/ and src/.)
-git checkout master doc test src
+# Switch to master branch (Sphinx build needs the checked out dirs and files).
+git checkout master doc test src setup.py README.rst
 git reset HEAD
+
+# Create a Python Virtualenv.
+virtualenv doc/venv
+doc/venv/bin/python setup.py install
+doc/venv/bin/pip install -r doc/requirements.txt
 
 # Build the docs and move html/ files root directory.
 cd doc
+
 venv/bin/python ./create.py build
 mv -fv build/html/* ..
 cd ..
 
 # Remove the directories (from master branch) needed for building docs.
-rm -rf doc test src
+rm -rf doc test src build README.rst setup.py
 
 # Add everything to gh-pages.
-git add .
+git add --all .
 
 # Commit with comment referencing latest master branch commit.
 git commit -m "Updated gh-pages for `git log master -1 | head -1`"
