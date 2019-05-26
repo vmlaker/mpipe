63d688d74c9c3a6d6a760d617c9fe231083534ae
vmlaker <velimir.mlaker@gmail.com>
2019-05-26 16:24:14 -0700

    Add logging to the homegrown test harness.

--- test/test.py
+++ test/test.py
@@ -2,12 +2,24 @@
 run the corresponding test, then do a simple line-by-line 
 comparison."""
 
-import sys
-import os
 import inspect
+import logging
+import os
 from subprocess import Popen, STDOUT, PIPE
+import sys
 
-# Compute the absolute path to directory of this file.
+
+# If command-line parameter is one of DEBUG, INFO, WARNING, etc. (lower case
+# accepted) then use the parameter to set the log level, otherwise ignore it.
+try:
+    logging.getLogger().setLevel(
+        eval('logging.{}'.format(sys.argv[1].upper())))
+except:
+    pass
+
+logging.basicConfig(format='%(levelname)-8s | %(message)s')
+
+# Compute the absolute path of the directory containing this file.
 this_dir = os.path.dirname(
     os.path.abspath(
         inspect.getfile(
@@ -15,41 +27,45 @@ this_dir = os.path.dirname(
 
 for entry in sorted(os.listdir(this_dir)):
 
-    # Identify the file -- whether it is gold.
-    # Gold files are *.gold for exact gold content, or 
-    # *.goldr if "random" (to be sorted before comparing.
+    # Decide whether this entry is a gold file. There are two types, based
+    # on filename suffix:
+    # 1) *.gold files contain the exact content expected from test output.
+    # 2) *.goldr files contain expected output, only "randomized."
+    #    I.e. we expect the test to produce the same content, but the lines
+    #    may not be in the same order.
     gold_fname = os.path.join(this_dir, entry)
     suffix = None
     for choice in ('.gold', '.goldr'):
         if gold_fname[-len(choice):] == choice:
             suffix = choice
     if not suffix:
+        logging.debug('Ignoring %s, not a gold file.', entry)
         continue
 
     # Read the gold file.
-    f = open(gold_fname)
-    gold_lines = f.readlines()
-    f.close()
+    with open(gold_fname) as f:
+        gold_lines = f.readlines()
+    logging.debug('Gold file %s has %d lines.', gold_fname, len(gold_lines))
 
     # Run the test.
     test_fname = gold_fname[:-len(suffix):] + '.py'
-    command = '{0} {1}'.format(sys.executable, test_fname)
-    print(command)
+    command = '{} {}'.format(sys.executable, test_fname)
+    logging.info('Running command: %s', command)
     p = Popen(command, shell=True, stdout=PIPE, stderr=STDOUT)
     test_lines = p.stdout.readlines()
-
-    # If test "randomizes" output, sort lines before comparing.
+    logging.debug('Test output has %d lines.', len(test_lines))
+    
+    # In case of "randomized" test output, sort lines before comparing.
     if suffix == '.goldr':
         gold_lines = sorted(gold_lines)
         test_lines = sorted(test_lines)
 
     # Compare line by line.
     failed = False
-    for gline, tline in zip(gold_lines, test_lines):
-        gline = gline.strip()
-        tline = tline.strip().decode()
-        if gline != tline:
-            print('Error running: {0}'.format(command))
+    for gold_line, test_line in zip(gold_lines, test_lines):
+        gold_line, test_line = gold_line.strip(), test_line.strip().decode()
+        if gold_line != test_line:
+            print('Error running: {}'.format(command))
             failed = True            
             break
 
